96dc00aa907316af4851f1d367b76ca0014d9622
Johan Beronius <johan.beronius@athega.se>
2018-03-09 18:11:08 +0100

    3D scan - PTS - LAS - Potree

---
+++ pointclouds/athegaKonf/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 488858,
+    "boundingBox": {
+        "lx": -15.045,
+        "ly": -22.781,
+        "lz": -4.793,
+        "ux": 16.631,
+        "uy": 8.895,
+        "uz": 26.883
+    },
+    "tightBoundingBox": {
+        "lx": -15.045,
+        "ly": -22.781,
+        "lz": -4.793,
+        "ux": 4.021,
+        "uy": 8.895,
+        "uz": 6.954
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.2743222117424011,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
